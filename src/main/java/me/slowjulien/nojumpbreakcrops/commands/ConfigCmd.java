6b4b72bbe927638d8eda9e1136e10b2a28d58e75
SlowJulien <slowjulien@protonmail.com>
2022-11-14 11:12:22 +0100

    Fixed a bug where operators perms were not taken into account

--- src/main/java/me/slowjulien/nojumpbreakcrops/commands/ConfigCmd.java
+++ src/main/java/me/slowjulien/nojumpbreakcrops/commands/ConfigCmd.java
@@ -11,12 +11,12 @@ import java.util.List;
 
 public class ConfigCmd extends NJBCCommand {
 
-    public ConfigCmd(JavaPlugin plugin) { super(plugin); }
+    public ConfigCmd(JavaPlugin plugin) {
+        super(plugin);
+    }
 
-    private static final String msgWrongArgs = "&cA problem happened with the args use :" +
-            "\n- /njbc" +
-            "\n- /njbc <enabled | disabled>" +
-            "\n- /njbc affectop <true | false>";
+    private static final String msgWrongArgs = "&cA problem happened with the args use :" + "\n- /njbc" + "\n- /njbc " +
+            "<enabled | disabled>" + "\n- /njbc affectop <true | false>";
     private static final String alrEnabled = "&cThe plugin is already &5&nenabled&r&c.";
     private static final String alrDisabled = "&cThe plugin system is already &5&ndisabled&r&c.";
     private static final String alrAffecOp = "&cOperators are &nalready&r&c affected by the plugin !";
@@ -26,7 +26,8 @@ public class ConfigCmd extends NJBCCommand {
     private static final String msgSuccAffOpTrue = "&aThe default operators are now &5affected &aby the plugin.";
     private static final String msgSuccAffOpFalse = "&aThe default operators are now &5bypassing &athe plugin.";
     private static final String sep = "&7======================";
-    private static final String infoMess = sep+"\n &aNJBC Status :\n   - &9Enabled &a: &5%s\n   &a- &9Affect Op &a: &5%s\n"+sep;
+    private static final String infoMess = sep + "\n &aNJBC Status :\n   - &9Enabled &a: &5%s\n   &a- &9Affect Op &a:" +
+            " &5%s\n" + sep;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -43,13 +44,13 @@ public class ConfigCmd extends NJBCCommand {
             // Current State
             boolean curState = (boolean) getPlugin().getConfig().get("enabled");
 
-            if(newState == curState) {
-                if(curState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', alrEnabled));
+            if (newState == curState) {
+                if (curState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', alrEnabled));
                 else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', alrDisabled));
             } else {
                 getPlugin().getConfig().set("enabled", newState);
                 getPlugin().saveConfig();
-                if(newState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccessEna));
+                if (newState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccessEna));
                 else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccessDis));
 
             }
@@ -61,25 +62,25 @@ public class ConfigCmd extends NJBCCommand {
             boolean newState = (args[1].equals("true"));
             // Current State
             boolean curState = (boolean) getPlugin().getConfig().get("affectop");
-            if(newState == curState) {
-                if(curState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', alrAffecOp));
+            if (newState == curState) {
+                if (curState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', alrAffecOp));
                 else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', notAlrAffecOp));
             } else {
                 getPlugin().getConfig().set("affectop", newState);
                 getPlugin().saveConfig();
-                if(newState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccAffOpTrue));
+                if (newState) sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccAffOpTrue));
                 else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgSuccAffOpFalse));
             }
         }
         // Something is wrong in the args
-        else sender.sendMessage(ChatColor.translateAlternateColorCodes('&',msgWrongArgs));
+        else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msgWrongArgs));
 
         return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if(args.length == 1) {
+        if (args.length == 1) {
             return new ArrayList<>(Arrays.asList("enable", "disable", "affectop"));
         } else if (args.length == 2 && args[0].equals("affectop")) {
             return new ArrayList<>(Arrays.asList("true", "false"));
--- src/main/java/me/slowjulien/nojumpbreakcrops/events/CropBreak.java
+++ src/main/java/me/slowjulien/nojumpbreakcrops/events/CropBreak.java
@@ -11,32 +11,39 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CropBreak implements Listener {
 
     private final JavaPlugin plugin;
+    private final String permission;
 
     public CropBreak(JavaPlugin plugin) {
         super();
         this.plugin = plugin;
+        this.permission = "njbc.breakcrops";
     }
 
     @EventHandler
     public void onBreakCrop(PlayerInteractEvent e) {
         // If the plugin is disabled
-        if(!((boolean) plugin.getConfig().get("enabled"))) return;
+        if (!((boolean) plugin.getConfig().get("enabled"))) return;
 
-        if(e.getAction() == Action.PHYSICAL) {
+        if (e.getAction() == Action.PHYSICAL) {
             Player p = e.getPlayer();
             try {
-                if(e.getClickedBlock().getType() == Material.FARMLAND) {
+                if (e.getClickedBlock().getType() == Material.FARMLAND) {
 
-                     if(!p.hasPermission("njbc.breakcrops")){
+                    // If permission is explicitly set to false
+                    if (p.isPermissionSet(this.permission) && !p.hasPermission(this.permission)) {
                         e.setCancelled(true);
-                    } else if(p.isOp()) {
-                         if(((boolean) plugin.getConfig().get("affectop")) && !p.hasPermission("njbc.breakcrops")) {
-                             e.setCancelled(true);
-                         }
+                    } else if (!p.hasPermission(this.permission)) { // The player does not have the perm...
+                        // If the player is not an operator OR
+                        // If the player is an operator AND the plugin is affecting operators
+                        if (!p.isOp() || (p.isOp() && ((boolean) plugin.getConfig().get("affectop")))) {
+                            e.setCancelled(true);
+                        }
                     }
 
                 }
-            } catch (NullPointerException ignored) {}
+            } catch (NullPointerException ignored) {
+            }
+
         }
     }
 
